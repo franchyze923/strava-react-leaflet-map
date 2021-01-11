433951fa063d3e5eb235f55efe9ca5e5e6f7639b
Fran Polignano <franchyze923@gmail.com>
2021-01-11 07:45:23 -0500

    got strava endpoint working
    got strava endpoint working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 import teslaData from "./data/tesla-sites.json"
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    async function fetchData() {
+      const stravaResponse = await axios.all([
+        // this token was generated after getting a new authoirzation code which gave us read_all permission. 
+        axios.get("https://www.strava.com/api/v3/athlete/activities?access_token={your token with read_all permission}")
+      ]);
+      console.log(stravaResponse)
+    }
+    fetchData();
+  }, []);
 
 const filteredStations = teslaData.filter(tsla => tsla.address.country === "Italy")
 
