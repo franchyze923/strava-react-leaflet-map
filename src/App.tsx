730ff750c5548d5ae882b962b0beee55be3b9017
Fran Polignano <franchyze923@gmail.com>
2021-01-11 17:42:57 -0500

    working strava api request with refresh token
    working strava api request with refresh token and data on the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 import teslaData from "./data/tesla-sites.json"
@@ -6,16 +6,35 @@ import axios from 'axios';
 
 function App() {
 
+  interface Node {
+    name: string;
+    id: number;
+    end_latlng: number[];
+  }
+
+  const [nodes, setNodes] = useState<Node[]>([]);
+
+  const clientID = "43995";
+  const clientSecret = "6b583c87a9969f7cc66c7b6b0cff643b820bb135";
+  const refreshToken = "98b596ec1ce3a93f6cda9515ee3cb430d259e946"
+  const auth_link = "https://www.strava.com/oauth/token"
+  const activities_link = `https://www.strava.com/api/v3/athlete/activities`
+
   useEffect(() => {
     async function fetchData() {
-      const stravaResponse = await axios.all([
-        // this token was generated after getting a new authoirzation code which gave us read_all permission. 
-        axios.get("https://www.strava.com/api/v3/athlete/activities?access_token={your token with read_all permission}")
+      const stravaAuthResponse = await axios.all([
+        axios.post(`${auth_link}?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`)
       ]);
-      console.log(stravaResponse)
+
+      console.log(stravaAuthResponse)
+      const stravaActivityResponse = await axios.get(`${activities_link}?access_token=${stravaAuthResponse[0].data.access_token}`);
+      console.log(stravaActivityResponse.data[0]);
+      setNodes(stravaActivityResponse.data);
     }
+
     fetchData();
   }, []);
+
 
 const filteredStations = teslaData.filter(tsla => tsla.address.country === "Italy")
 
@@ -26,15 +45,15 @@ const filteredStations = teslaData.filter(tsla => tsla.address.country === "Ital
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {filteredStations.map(tsla => (
-        <Marker key = {tsla.id} position={[tsla.gps.latitude, tsla.gps.longitude]}>
-          <Popup position={[tsla.gps.latitude, tsla.gps.longitude]}>
+      {nodes.map(activity => (
+        <Marker key = {activity.id} position={[activity.end_latlng[0], activity.end_latlng[1]]}>
+          {/* <Popup position={[activity.end_latlng[0], activity.end_latlng[1]]}>
             <div>
               <h2>{"Name: " + tsla.name}</h2>
               <p>{"Status: " + tsla.status}</p>
               <p>{"Number of Charging Stations: " + tsla.stallCount}</p>
             </div>
-          </Popup>
+          </Popup> */}
         </Marker>
       ))}
     </MapContainer>
