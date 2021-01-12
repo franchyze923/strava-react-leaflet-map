af076bfbbf3a408c764c86d5566613f9fab1ed38
Fran Polignano <franchyze923@gmail.com>
2021-01-12 07:54:35 -0500

    strava activites now displaying on map !
    strava activites now displaying on map with popups

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Polyline } from 'react-leaflet'
 import './App.css';
-import teslaData from "./data/tesla-sites.json"
 import axios from 'axios';
+import polyline from '@mapbox/polyline'
 
 function App() {
 
   interface Node {
-    name: string;
-    id: number;
-    end_latlng: number[];
+    activityPositions: any;
+    activityName: string;
   }
 
   const [nodes, setNodes] = useState<Node[]>([]);
@@ -25,36 +24,38 @@ function App() {
       const stravaAuthResponse = await axios.all([
         axios.post(`${auth_link}?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`)
       ]);
-
-      console.log(stravaAuthResponse)
+      
       const stravaActivityResponse = await axios.get(`${activities_link}?access_token=${stravaAuthResponse[0].data.access_token}`);
       console.log(stravaActivityResponse.data[0]);
-      setNodes(stravaActivityResponse.data);
+      const polylines = [];
+      for (let i = 0; i < stravaActivityResponse.data.length; i += 1) {
+        const activity_polyline = stravaActivityResponse.data[i].map.summary_polyline;
+        const activity_name = stravaActivityResponse.data[i].name;
+        polylines.push({activityPositions: polyline.decode(activity_polyline), activityName: activity_name});
+      }
+      console.log(polylines)
+      setNodes(polylines);
     }
 
     fetchData();
   }, []);
 
-
-const filteredStations = teslaData.filter(tsla => tsla.address.country === "Italy")
-
   return (
+   
     <MapContainer center={[42.585444, 13.257684]} zoom={6} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {nodes.map(activity => (
-        <Marker key = {activity.id} position={[activity.end_latlng[0], activity.end_latlng[1]]}>
-          {/* <Popup position={[activity.end_latlng[0], activity.end_latlng[1]]}>
+      {nodes.map((activity, i) => (
+        <Polyline key = {i} positions={activity.activityPositions}>
+          <Popup>
             <div>
-              <h2>{"Name: " + tsla.name}</h2>
-              <p>{"Status: " + tsla.status}</p>
-              <p>{"Number of Charging Stations: " + tsla.stallCount}</p>
+              <h2>{"Name: " + activity.activityName}</h2>
             </div>
-          </Popup> */}
-        </Marker>
+          </Popup>
+        </Polyline>
       ))}
     </MapContainer>
   );
